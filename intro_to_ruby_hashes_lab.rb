2310b2f3d948dfb649f92eb3f65ef0714fb641bf
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-29 22:21:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ def monopoly_with_fourth_tier
       two_pieces_owned: 50,
       three_pieces_owned: 100,
       four_pieces_owned: 200,
-      reading_railroad: {
+      :reading_railroad =>{}
     
       }
     
