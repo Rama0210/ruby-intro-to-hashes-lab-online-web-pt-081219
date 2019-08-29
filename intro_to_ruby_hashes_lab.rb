e614a0cf3a0f1be90b1a2cb983b7e2b5b46b9091
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-29 21:36:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,12 +8,12 @@ end
 
 
 def monopoly
-	monopoly = {:"railroads" => {}}
+	monopoly = {"railroads": {}}
 
 end
 
 def monopoly_with_second_tier
-  monopoly = {:"railroads" => :"names" => []}
+  monopoly = {"railroads": []}
   
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
